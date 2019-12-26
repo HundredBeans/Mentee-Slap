ac534776b49406f7a3582bb5f8953a2828900dcc
Mohammad Daffa <daffa@alterra.id>
2019-12-26 22:38:18 +0700

    add some changes in html and js

--- tampol.js
+++ tampol.js
@@ -1,24 +1,37 @@
 let canvas = document.getElementById('field')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 let ctx = canvas.getContext('2d')
 
 
 canvas.onload="myMove"
 
 function startGame(){
+    canvas.style.visibility = 'visible'
     let startBtn = document.getElementById('start-game')
-    startBtn.innerHTML = 'RESTART'
-    start()
+    if (startBtn.innerHTML == 'STOP'){
+        stop()
+        startBtn.innerHTML = 'START'
+    }else{
+        startBtn.innerHTML = 'STOP'
+        loadInterval = window.setInterval(start, 600)
+    }
 }
 function start(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     loadImage()
 }
 function loadImage(){
-    let rand = Math.round(Math.random()*19)
+    let rand = Math.ceil(Math.random()*19)
     let image = new Image();
     image.onload = function(){
-    let x = Math.random()*innerWidth;
-    let y = Math.random()*innerHeight;
-    ctx.drawImage(image, x, y, 100, 100)
+    let x = Math.round(Math.random()*(canvas.width-200));
+    let y = Math.round(Math.random()*(canvas.height-200));
+    ctx.drawImage(image, x, y, 200, 200)
     }
 image.src = `../Mentee-Slap/assets/img/before/circle-cropped(${rand}).png`
+}
+function stop(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    window.clearInterval(loadInterval)
 }
