fe9c4f7b3401471c16fee55fab6f7f1d5239e00a
Achmad Miftahul Ulum <miftahul@alterra.id>
2019-12-26 23:22:34 +0700

    amulum

--- tampol.js
+++ tampol.js
@@ -12,13 +12,31 @@ function startGame(){
 function start(){
     loadImage()
 }
+let x = 10;
+let y = 10;
+let dx = 4;
+let dy = 4;
 function loadImage(){
-    let rand = Math.round(Math.random()*19)
+    requestAnimationFrame(loadImage)
+    let rand = 1
     let image = new Image();
     image.onload = function(){
-    let x = Math.random()*innerWidth;
-    let y = Math.random()*innerHeight;
-    ctx.drawImage(image, x, y, 100, 100)
+        
+        ctx.clearRect(0, 0, (innerWidth-100), (innerHeight));
+        ctx.drawImage(image, x, y, 100, 100);
+        if (x + 100 > canvas.width || x < 0) {
+            dx = -dx;
+        }
+        if (y + 100 > canvas.height || y < 0) {
+            dy = -dy;
+        }
+
+        // requestAnimationFrame(animate);
+        // x += 1;
     }
-image.src = `../Mentee-Slap/assets/img/before/circle-cropped(${rand}).png`
+    x += dx;
+    y += dy;
+
+
+    image.src = `../Mentee-Slap/assets/img/before/circle-cropped(${rand}).png`
 }
